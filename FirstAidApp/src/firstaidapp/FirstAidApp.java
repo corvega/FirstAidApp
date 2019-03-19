96b49e31cc02768e0d914e424e36f51c2bb1bb49
Felix <felixwmayo@gmail.com>
2019-03-19 11:51:14 +0000

    Update firstaidapp.class

--- FirstAidApp/src/firstaidapp/FirstAidApp.java
+++ FirstAidApp/src/firstaidapp/FirstAidApp.java
@@ -11,11 +11,4 @@ package firstaidapp;
  */
 public class FirstAidApp {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
 }
