1803225898cae16a5c4202879056863b6f459bc6
Felix <felixwmayo@gmail.com>
2019-03-20 10:39:49 +0000

    Changing to built in netbeans DB

--- FirstAidApp/src/main.java
+++ FirstAidApp/src/main.java
@@ -8,11 +8,28 @@
  *
  * @author felixmayo
  */
+
+import java.sql.*;
+
 public class main {
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+    public static final String CONN_STRING = "jdbc:derby://localhost:1527/firstAidAppDB";
+    
         /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Connection conn = null;
+        try{
+            conn = DriverManager.getConnection(CONN_STRING,USERNAME,PASSWORD);
+            System.out.println("Connected");
+            Statement stmt = (Statement) conn.createStatement();
+            String insert =  "INSERT INTO USERS (ID,FName,LName) VALUES (1,'Test','Tester')";
+            stmt.executeUpdate(insert);
+        }catch (SQLException e){
+            System.err.println(e);
+        }
         // TODO code application logic here
     } 
 }
