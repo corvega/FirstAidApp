a94a7d7df67713d0351a33a1b3024b0e0b11e01e
TLDRQwerty <charliecarolan1999@gmail.com>
2019-03-21 14:18:25 +0000

    Global variables and connectToDatabase function

--- FirstAidApp/src/Database.java
+++ FirstAidApp/src/Database.java
@@ -1,28 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.sql.*;
 
 /**
- *
- * @author felixmayo
+ * Database
  */
-import java.sql.*;
-import javax.swing.JOptionPane;
-
 public class Database {
-    public static Connection mariaDBConn(){
-        Connection Conn=null;
-        try{
-            Class.forName("org.mariadb.jdbc.Driver");
-            Conn = DriverManager.getConnection("jdbc:mariadb://localhost:3307/DB?user=root&password=root");
-            JOptionPane.showMessageDialog(null, "Success Login ..");
-            return Conn;
-        }
-        catch(Exception E){
-            JOptionPane.showMessageDialog(null, E);
-            return null;
+
+    private final String URL = "jdbc:mariadb://root@localhost/";
+    private final String USER = "root";
+    private final String PASSWORD = "root";
+    private final String DB_NAME = "inse";
+
+    /**
+     * This just creates a connection to the database to be used within this class.
+     */
+    private Connection connectToDatabase() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new Error(e.toString());
         }
     }
+
 }
