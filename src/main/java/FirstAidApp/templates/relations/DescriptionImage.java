fcc6f5446af6610683059901c541e3d01f5ec8ea
TLDRQwerty <charliecarolan1999@gmail.com>
2019-04-04 16:40:33 +0100

    Added documentation for the DescriptionImage class Removed useless setter methods

--- src/main/java/FirstAidApp/templates/relations/DescriptionImage.java
+++ src/main/java/FirstAidApp/templates/relations/DescriptionImage.java
@@ -2,40 +2,49 @@ package FirstAidApp.templates.relations;
 
 import java.io.File;
 
+/**
+ * This is the model class for having a description and an image.
+ */
 public class DescriptionImage {
 
     public File image;
     public String description;
     public String imageCaption;
-    /* TODO: Might split this into a nother type  */
 
+    /**
+     * Constructor
+     * @param description for the image
+     * @param pathToImage the file PATH to the image
+     * @param imageCaption the caption for the image
+     */
     public DescriptionImage(String description, String pathToImage, String imageCaption) {
         this.description = description;
         this.image = new File(pathToImage);
         this.imageCaption = imageCaption;
     }
 
+    /**
+     * Getter for the image
+     * @return the image file
+     */
     public File getImage() {
         return image;
     }
 
+    /**
+     * Getter for the description
+     * @return the description for the image
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Getter for the image caption
+     * @return the image caption
+     */
     public String getImageCaption() {
         return imageCaption;
     }
 
-    public void setImage(File image) {
-        this.image = image;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setImageCaption(String imageCaption) {
-        this.imageCaption = imageCaption;
-    }
 }
