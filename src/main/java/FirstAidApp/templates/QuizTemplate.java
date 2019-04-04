d079750a5fd3e11f8ec4add3159f20bcb59d0a92
TLDRQwerty <charliecarolan1999@gmail.com>
2019-04-04 16:40:08 +0100

    Added documentation for the quiz class

--- src/main/java/FirstAidApp/templates/QuizTemplate.java
+++ src/main/java/FirstAidApp/templates/QuizTemplate.java
@@ -4,17 +4,33 @@ import FirstAidApp.templates.relations.Answers;
 
 import java.util.ArrayList;
 
+/**
+ * Model for the Quiz
+ */
 public class QuizTemplate {
     String title;
     String question;
     Answers answers;
 
+    /**
+     * Constructor
+     * @param title for the quiz
+     * @param question for the quiz
+     * @param answers for the quiz
+     */
     public QuizTemplate(String title, String question, Answers answers) {
         this.title = title;
         this.question = question;
         this.answers = answers;
     }
 
+    /**
+     * Constructor
+     * @param title for the quiz
+     * @param question for the quiz
+     * @param correctAnswer for the quiz
+     * @param fakeAnswers for the quiz
+     */
     public QuizTemplate(String title, String question, String correctAnswer, ArrayList<String> fakeAnswers) {
         if (fakeAnswers.size() != 3) throw new Error("Fake Answer array must be of length 3");
         this.title = title;
@@ -22,42 +38,68 @@ public class QuizTemplate {
         this.answers = new Answers(correctAnswer, fakeAnswers);
     }
 
+    /**
+     * Constructor
+     * @param title for the quiz
+     * @param question for the quiz
+     * @param correctAnswer for the quiz
+     * @param fakeAnswer1 for the quiz
+     * @param fakeAnswer2 for the quiz
+     * @param fakeAnswer3 for the quiz
+     */
     public QuizTemplate(String title, String question, String correctAnswer, String fakeAnswer1, String fakeAnswer2, String fakeAnswer3) {
         this.title = title;
         this.question = question;
         this.answers = new Answers(correctAnswer, fakeAnswer1, fakeAnswer2, fakeAnswer3);
     }
 
+    /**
+     * Getter for the title
+     * @return title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Getter for the question
+     * @return question
+     */
     public String getQuestion() {
         return question;
     }
 
+    /**
+     * Getter for the answers
+     * @return answers
+     */
     public Answers getAnswers() {
         return answers;
     }
 
+    /**
+     * Getter for the fake answers
+     * @return fake answers
+     */
     public ArrayList<String> getFakeAnswers() {
         return this.answers.getFakeAnswers();
     }
 
+    /**
+     * Getter for a fake answer
+     * @param index of fake answer that is wanted to be returned.
+     * @return fake answer
+     */
     public String getFakeAnswer(int index) {
         return this.answers.getFakeAnswer(index);
     }
 
+    /**
+     * Getter for the correct answer
+     * @return the correct answer
+     */
     public String getCorrectAnswer() {
         return this.answers.getCorrectAnswer();
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
     }
 }
 
