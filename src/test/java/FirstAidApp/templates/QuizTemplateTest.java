81f7d9cea9510f695873776df15ee15c04674a71
TLDRQwerty <charliecarolan1999@gmail.com>
2019-04-04 18:51:57 +0100

    Added more testing to the quiz template test class

--- src/test/java/FirstAidApp/templates/QuizTemplateTest.java
+++ src/test/java/FirstAidApp/templates/QuizTemplateTest.java
@@ -2,7 +2,10 @@ package FirstAidApp.templates;
 
 
 
+import FirstAidApp.templates.relations.Answers;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -13,17 +16,40 @@ public class QuizTemplateTest {
     public void getTitle() {
         QuizTemplate quizTemplate = new QuizTemplate("a", "b", "c", "d", "e", "f");
         assertEquals(quizTemplate.getTitle(), "a");
+
+        Answers answers = new Answers("c", "d", "e", "f");
+        QuizTemplate quiz = new QuizTemplate("a", "b", answers);
+        assertEquals(quiz.getTitle(), "a");
     }
 
     @Test
     public void getQuestion() {
         QuizTemplate quizTemplate = new QuizTemplate("a", "b", "c", "d", "e", "f");
         assertEquals(quizTemplate.getQuestion(), "b");
+
+        Answers answers = new Answers("c", "d", "e", "f");
+        QuizTemplate quiz = new QuizTemplate("a", "b", answers);
+        assertEquals(quiz.getQuestion(), "b");
+    }
+
+    @Test
+    public void getFakeAnswers() {
+        QuizTemplate quiz = new QuizTemplate("a","b","c","d","e","f");
+        ArrayList<String> fakeAnswers = new ArrayList();
+        fakeAnswers.add("d");
+        fakeAnswers.add("e");
+        fakeAnswers.add("f");
+        Answers answers = new Answers("c", fakeAnswers);
+        assertEquals(quiz.getFakeAnswers().equals(fakeAnswers), true);
     }
 
     @Test
     public void getFakeAnswer() {
         QuizTemplate quizTemplate = new QuizTemplate("a", "b", "c", "d", "e", "f");
         assertEquals(quizTemplate.getFakeAnswer(0), "d");
+
+        Answers answers = new Answers("c", "d", "e", "f");
+        QuizTemplate quiz = new QuizTemplate("a", "b", answers);
+        assertEquals(quiz.getFakeAnswer(0), "b");
     }
 }
